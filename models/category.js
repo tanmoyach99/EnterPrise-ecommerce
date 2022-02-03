ac73cdb978608deca5f1db247cc10690da0b2a86
tanmoyach99e <tanmoyacharjee18@gmail.comm>
2022-02-03 22:36:07 +0600

    server core part done

---
+++ models/category.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// const { objectId } = mongoose.Schema;
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: "String",
+      unique: true,
+      index: true,
+      trim: true,
+      required: "Name is required",
+      minlength: [2, "Too short"],
+      maxlength: [32, "Too long"],
+    },
+    slug: {
+      type: "String",
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// const authRoutes = require("./routes/auth");
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(`db connection err`, err));
+
+//middleware
+
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+//routes middleware
+// app.use("/api", authRoutes);
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+//router routes
+// app.get("/api", (req, res) => {
+//   res.json({
+//     data: "what..........??",
+//   });
+// });
+
+app.listen(port, () => console.log(`server is running on ${port}`));
