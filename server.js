0e8e47bb4d0ce2f233a1679e8462b83448a7dff5
tanmoyach99e <tanmoyacharjee18@gmail.comm>
2022-06-03 14:27:32 +0600

    few error fixed

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.send("app is working");
+});
 
 // const authRoutes = require("./routes/auth");
 
